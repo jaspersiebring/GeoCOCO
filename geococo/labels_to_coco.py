b4e50d9c98ab2f32b1f49367e964030150558119
Jasper <j.siebring92@gmail.com>
2023-08-25 16:31:11 +0200

    Moving id adminstration to CocoDataset, switched to pydantic's dataclass for WindowSource, general refactoring.

--- geococo/labels_to_coco.py
+++ geococo/labels_to_coco.py
@@ -2,7 +2,7 @@ import pathlib
 import tempfile
 import uuid
 from datetime import datetime
-from typing import Dict, Generator, List, Optional, Tuple
+from typing import List, Tuple
 import cv2
 import geopandas as gpd
 import numpy as np
@@ -11,32 +11,28 @@ from pycocotools import mask as cocomask
 from rasterio.io import DatasetReader
 from rasterio.mask import mask as riomask
 from rasterio.transform import array_bounds
-from rasterio.windows import Window
 from shapely.geometry import box
 from tqdm import tqdm
-from geococo.models import Annotation, Category, CocoDataset, Image, Info, WindowSource
-from geococo.utils import estimate_window_source, generate_window_offsets, window_factory, dump_dataset, load_dataset
+from geococo.models import Annotation, Category, CocoDataset, Image, Info
+from geococo.utils import estimate_window_source, generate_window_offsets, window_factory
 
 
-def populate_coco_dataset(dataset: CocoDataset, images_dir: pathlib.Path, src: DatasetReader, labels: gpd.GeoDataFrame, window_bounds:  List[Tuple[int, int]]) -> CocoDataset:
-    image_ids = [image.id for image in dataset.images]
-    annotation_ids = [annotation.id for annotation in dataset.annotations]
-    next_image_id = 1 if not image_ids  else np.max(image_ids)
-    next_annotation_id = 1 if not annotation_ids else np.max(annotation_ids)
-    
+def populate_coco_dataset(dataset: CocoDataset, images_dir: pathlib.Path, src: DatasetReader, labels: gpd.GeoDataFrame, window_bounds:  List[Tuple[int, int]], normalize: bool = True) -> CocoDataset:    
+
+    # Setting nodata and estimating window configuration
     dst_profile = src.profile.copy()
-    nodata_value = src.nodata if src.nodata else 0
-    
+    nodata_value = src.nodata if src.nodata else 0    
     window_source = estimate_window_source(gdf=labels, src=src, window_bounds = window_bounds)
     parent_window = src.window(*src.bounds)
     n_windows = generate_window_offsets(window = parent_window, window_source = window_source).shape[0]
 
-    geoms = []
+    geoms = [] 
     for child_window in tqdm(window_factory(parent_window=parent_window, window_source=window_source), total=n_windows):
+
+        # changing window to Shapely.geometry, used to check for intersecting labels
         window_transform = src.window_transform(child_window)
         window_bounds = array_bounds(child_window.height, child_window.width, window_transform)
         window_geom = box(*window_bounds)
-
         intersect_mask = labels.intersects(window_geom)
         if not intersect_mask.any():
             continue
@@ -52,16 +48,20 @@ def populate_coco_dataset(dataset: CocoDataset, images_dir: pathlib.Path, src: D
             nodata=nodata_value
         )
         
-        # padding is needed because rasterio clips any mask to the extent of the given datasource 
+        # padding is needed because rasterio clips any mask to the extent of the given datasource
         if window_image.shape != (src.count, child_window.width, child_window.height):
             window_image = window_image[:src.count, :child_window.width, :child_window.height]
             pad_dims = [(0, max(0, n - window_image.shape[i])) for i, n in enumerate([src.count, child_window.width, child_window.height])]
             window_image = np.pad(window_image, pad_dims, mode='constant', constant_values=nodata_value)
-                
-        # updating profile with changed values
+        
+        # normalizing values to uint8 range (i.e COCO dtype) 
+        if window_image.dtype != np.uint8 and normalize:
+            window_image = cv2.normalize(window_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # updating profile with changed values      
         dst_profile.update(
             {
-                'dtype': window_image.dtype,
+                'dtype': np.uint8,
                 'width': window_image.shape[1],
                 'height': window_image.shape[2],
                 'nodata': nodata_value,
@@ -69,25 +69,25 @@ def populate_coco_dataset(dataset: CocoDataset, images_dir: pathlib.Path, src: D
                 "driver": "JPEG"
             }
         )
-
-        window_image_path = images_dir / f"{child_window.col_off}_{child_window.row_off}_{child_window.width}_{child_window.height}.jpg"
         
+        # saving window_image to disk (if it doesn't exist already)
+        window_image_path = images_dir / f"{child_window.col_off}_{child_window.row_off}_{child_window.width}_{child_window.height}.jpg"
         if not window_image_path.exists():
             with rasterio.open(window_image_path, 'w', **dst_profile) as dst:
                 dst.write(window_image)
-    
+        
+        # Instancing and adding Image model to dataset (also bumps next_image_id) 
         image_instance = Image(
-            id=next_image_id,
+            id=dataset.next_image_id,
             width=window_image.shape[1],
             height=window_image.shape[2],
             file_name = window_image_path)
+        dataset.add_image(image=image_instance)
         
-        dataset.images.append(image_instance)
-        
-        # adding Annotation objects
+        # Iteratively add Annotation models to dataset (also bumps next_annotation_id)
         with rasterio.open(window_image_path) as windowed_src:
             for _, intersecting_label in intersecting_labels.sort_values('category_id').iterrows():
-                intersected_image, intersected_transform = riomask(
+                intersected_image, _ = riomask(
                     dataset=windowed_src,
                     shapes = [intersecting_label.geometry], 
                     all_touched=True,
@@ -101,73 +101,17 @@ def populate_coco_dataset(dataset: CocoDataset, images_dir: pathlib.Path, src: D
                 area = np.sum(intersected_mask)
                 category_id = intersecting_label['category_id']
                 iscrowd = 0 # TODO determined by multipolygon
+
                 annotation_instance = Annotation(
-                    id=next_annotation_id,
-                    image_id = next_image_id,
+                    id = dataset.next_annotation_id,
+                    image_id = dataset.next_image_id,
                     category_id=category_id,
                     segmentation=rle,
                     area=area,
                     bbox = bounding_box,
                     iscrowd=iscrowd)
+                dataset.add_annotation(annotation=annotation_instance)
                 
-                annotation_instance
-                dataset.annotations.append(annotation_instance)
-                next_annotation_id += 1
-
-        next_image_id += 1
         geoms.append(window_geom)
 
     return dataset, geoms
-
-
-    
-if __name__ == "__main__":
-    image_path = pathlib.Path("D:/repos/GeoCOCO/data/image.tif")
-    label_path = pathlib.Path("D:/repos/GeoCOCO/data/labels.shp")
-    
-    labels: gpd.GeoDataFrame = gpd.read_file(label_path)
-    labels['category_id'] = np.random.randint(1, 10, labels.shape[0]) #faking categories
-    src: DatasetReader = rasterio.open(image_path)
-    
-    window_bounds = [(256, 256), (512, 512)]
-    raster_bounds = gpd.GeoSeries(box(*src.bounds), crs=src.crs)
-    label_bounds = gpd.GeoSeries(box(*labels.total_bounds), crs=labels.crs)
-        
-    if not src.crs.to_string() == labels.crs.to_string():
-        raise ValueError("Projection of given spatial objects don't match, exiting..")
-
-    #check for overlapping bounds (requirement)
-    if not all(raster_bounds.intersects(label_bounds)):
-        raise ValueError("There's no overlap between input objects, exiting..")
-
-
-    output_dir_str = tempfile.TemporaryDirectory(prefix=f"output_{uuid.uuid4()}")
-    output_dir = pathlib.Path(output_dir_str.name)
-
-    print(output_dir)
-
-    # Instancing new dataset
-    info = Info(version = "0.0.1", date_created=datetime.now())
-    dataset = CocoDataset(
-        info=info,
-        images = [],
-        annotations = [],
-        categories=[]
-        )
-
-    
-    dataset, geoms = populate_coco_dataset(
-        dataset = dataset, 
-        images_dir = output_dir,
-        src = src,
-        labels = labels,
-        window_bounds = window_bounds
-        )
-    
-    
-    gpd_path = output_dir / "geoms.shp"
-    gpd.GeoDataFrame(geometry = geoms, crs = src.crs).to_file(gpd_path)
-    
-        
-    json_path = output_dir / "first_dataset.json"
-    dump_dataset(dataset=dataset, json_path=json_path)
--- geococo/models.py
+++ geococo/models.py
@@ -1,23 +1,18 @@
-from pydantic import BaseModel
-from dataclasses import dataclass
+from __future__ import annotations
+from pydantic.dataclasses import dataclass
+from pydantic import BaseModel, ConfigDict, Field, AnyUrl, InstanceOf, model_validator
 from typing import Optional, List, Dict
 from datetime import datetime
 import pathlib
-# from panopticapi.utils import save_json # check for formatting 
 
 @dataclass
 class WindowSource:
-    width_window_pixels: int
-    width_overlap_pixels: int
-    height_window_pixels: int
-    height_overlap_pixels: int
+    width_window_pixels: int = Field(gt=0)
+    width_overlap_pixels: int = Field(ge=0)
+    height_window_pixels: int = Field(gt=0)
+    height_overlap_pixels: int = Field(ge=0)
     
     def __post_init__(self):
-        # type checking (maybe just use pydantic?)
-        for var_name, var_type in self.__annotations__.items():
-            if not isinstance(getattr(self, var_name), var_type):
-                raise ValueError(f"self.{var_name} is not of required type {var_type}")
-
         self.width_step_pixels: int = (
             self.width_window_pixels - self.width_overlap_pixels * 2
         )
@@ -37,40 +32,65 @@ class WindowSource:
             raise ValueError(
                 f"height_step_pixels ({self.height_step_pixels}) must be positive, increase height_window_pixels ({self.height_overlap_pixels}) or decrease height_overlap_pixels ({self.height_overlap_pixels})"
             )
-        
+
 
 class Info(BaseModel):
     year: Optional[int] = None
     version: Optional[str] = None
     description: Optional[str] = None
     contributor: Optional[str] = None
-    url: Optional[str] = None
+    url: Optional[AnyUrl] = None
     date_created: Optional[datetime] = None
 
 class Image(BaseModel):
+    model_config = ConfigDict(frozen=True)
     id: int 
     width: int
     height: int
     file_name: pathlib.Path
-    date_captured: Optional[datetime] = None
 
 class Annotation(BaseModel):
+    model_config = ConfigDict(frozen=True)
     id: int
     image_id: int
     category_id: int
     segmentation: Dict
-    area: int
-    bbox: List[int] 
+    area: float
+    bbox: List[float] 
     iscrowd: int
 
 class Category(BaseModel):
+    model_config = ConfigDict(frozen=True)
     id: int
     name: str
     supercategory: str
 
-# add read_from and save_as methods
 class CocoDataset(BaseModel):
     info: Info
-    images: List[Image]
-    annotations: List[Annotation]
-    categories: List[Category]
+    images: List[InstanceOf[Image]] = []
+    annotations: List[InstanceOf[Annotation]] = []
+    categories: List[InstanceOf[Category]] = []
+    _next_image_id: int = 1
+    _next_annotation_id: int = 1
+    
+    @model_validator(mode="after")
+    def _set_ids(self) -> CocoDataset:
+        self._next_image_id = len(self.images) + 1
+        self._next_annotation_id = len(self.annotations) + 1
+        return self
+        
+    def add_annotation(self, annotation: Annotation) -> None:
+        self.annotations.append(annotation)
+        self._next_annotation_id += 1
+
+    def add_image(self, image: Image) -> None:
+        self.images.append(image)
+        self._next_image_id += 1
+
+    @property
+    def next_image_id(self) -> int: 
+        return self._next_image_id
+    
+    @property
+    def next_annotation_id(self) -> int:
+        return self._next_annotation_id
