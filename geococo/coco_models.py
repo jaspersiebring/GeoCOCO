b609b1ac366f3059e819ee30d2610b7de06d721e
Jasper <j.siebring92@gmail.com>
2023-09-07 15:42:41 +0200

    Making CocoDataset superset by adding Sources (i.e. source for all clipped images). Needed for proper SemVer.

--- geococo/coco_models.py
+++ geococo/coco_models.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import pathlib
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Set
 from typing_extensions import TypedDict
 from pydantic import BaseModel, ConfigDict, InstanceOf, model_validator
 
@@ -11,13 +11,18 @@ class CocoDataset(BaseModel):
     images: List[InstanceOf[Image]] = []
     annotations: List[InstanceOf[Annotation]] = []
     categories: List[InstanceOf[Category]] = []
+    sources: List[InstanceOf[Source]] = []
     _next_image_id: int = 1
     _next_annotation_id: int = 1
-
+    _next_source_id: int = 1
+    _unique_output_dirs: Set[pathlib.Path] = set()
+    _unique_source_images : Set[pathlib.Path] = set()
+    
     @model_validator(mode="after")
     def _set_ids(self) -> CocoDataset:
         self._next_image_id = len(self.images) + 1
         self._next_annotation_id = len(self.annotations) + 1
+        self._next_source_id = len(self.sources) + 1
         return self
 
     def add_annotation(self, annotation: Annotation) -> None:
@@ -28,6 +33,10 @@ class CocoDataset(BaseModel):
         self.images.append(image)
         self._next_image_id += 1
 
+    def add_source(self, source: Source) -> None:
+        self.sources.append(source)
+        self._next_source_id += 1
+    
     @property
     def next_image_id(self) -> int:
         return self._next_image_id
@@ -35,6 +44,10 @@ class CocoDataset(BaseModel):
     @property
     def next_annotation_id(self) -> int:
         return self._next_annotation_id
+    
+    @property
+    def next_source_id(self) -> int:
+        return self._next_source_id
 
 
 class Info(BaseModel):
@@ -51,6 +64,7 @@ class Image(BaseModel):
     width: int
     height: int
     file_name: pathlib.Path
+    source_id: int
 
 
 class Annotation(BaseModel):
@@ -74,3 +88,8 @@ class Category(BaseModel):
 class RleDict(TypedDict):
     size: List[int]
     counts: bytes
+
+
+class Source(BaseModel):
+    source_id: int
+    file_name: pathlib.Path
